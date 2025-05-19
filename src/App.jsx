4734f9083ad2daba75adcde700ddcd0b2911c450
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-05-19 16:58:32 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar.jsx";
 import Hero from "./sections/hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <ShowcaseSection />
     </>
