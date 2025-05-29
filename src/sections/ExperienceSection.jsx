b5aaf06433c0f6422c43d9c384f18aa506b74f7f
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-05-29 22:50:02 +0530

    update experiencesection

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import TitleHeader from "../components/TitleHeader.jsx";
+import { expCards } from "../constants/index.js";
 
 const ExperienceSection = () => {
   return (
@@ -6,7 +8,21 @@ const ExperienceSection = () => {
       id="experience"
       className="w-full mdd:mt-40 mt-20 section-padding xl:px-0"
     >
-      <div></div>
+      <div className="w-full h-full md:px-20 px-5">
+        <TitleHeader
+          title="Professional Work Experience "
+          sub="My Career Overview"
+        />
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            {expCards.map((Card) => (
+              <div key={Card.title} className="exp-card-wrapper">
+                {Card.title}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
